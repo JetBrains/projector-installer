e99cab5900044c67e0e2d96aa2688d3c4fdc991e
Ilya Krukov <ikrukov@horis.ru>
2020-07-07 16:56:05 +0300

    License changed to GPLv2

--- projector/dialogs.py
+++ projector/dialogs.py
@@ -1,3 +1,20 @@
+#  GNU General Public License version 2
+#
+#  Copyright (C) 2019-2020 JetBrains s.r.o.
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License version 2 only, as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import click
 from .apps import get_installed_apps, get_app_path, get_compatible_app_names
 from .run_config import get_run_configs, RunConfig, get_used_http_ports, get_used_projector_ports, get_run_config_names
