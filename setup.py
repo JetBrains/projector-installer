2138e44f7492688f38c2ca683fc0ce2338691293
Ilya Krukov <ikrukov@horis.ru>
2020-07-09 20:40:59 +0300

    Style fixes

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+"""projector-installer setup file."""
+
 from setuptools import setup
 
 with open('requirements.txt') as f:
