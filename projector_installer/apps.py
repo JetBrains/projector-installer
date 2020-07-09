57565c967eca83a120889f0399171f16f8080f40
Ilya Krukov <ikrukov@horis.ru>
2020-07-09 19:13:26 +0300

    Add pylintrc. Style fixes

--- projector_installer/apps.py
+++ projector_installer/apps.py
@@ -15,10 +15,13 @@
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from dataclasses import dataclass
+"""Application management functions."""
+
 from os.path import join, expanduser, dirname
 from os import listdir, chmod, stat
 import json
+from dataclasses import dataclass
+
 from .global_config import get_apps_dir, PROJECTOR_SERVER_DIR, COMPATIBLE_APPS
 from .utils import unpack_tar_file
 
@@ -29,10 +32,12 @@ IDEA_PATH_SELECTOR = 'idea.paths.selector'
 
 
 def unpack_app(file_path):
+    """Unpacks specified file to app directory."""
     return unpack_tar_file(file_path, get_apps_dir())
 
 
 def get_installed_apps(pattern=None):
+    """Returns sorted list of installed apps, matched given pattern."""
     res = [file_name for file_name in listdir(get_apps_dir()) if
            pattern is None or file_name.lower().find(pattern.lower()) != -1]
     res.sort()
@@ -40,11 +45,12 @@ def get_installed_apps(pattern=None):
 
 
 def get_compatible_apps(pattern=None):
+    """Returns list of compatible apps, matched given pattern."""
     apps = [app for app in COMPATIBLE_APPS if
             pattern is None or app.name.lower().find(pattern.lower()) != -1]
 
     if pattern:
-        for app in apps:  # handle exact match
+        for app in apps:
             if pattern.lower() == app.name.lower():
                 return [app]
 
@@ -52,16 +58,19 @@ def get_compatible_apps(pattern=None):
 
 
 def get_compatible_app_names(pattern=None):
+    """Get sorted list of projector-compatible applications, matches given pattern."""
     res = [app.name for app in get_compatible_apps(pattern)]
     res.sort()
     return res
 
 
 def get_app_path(app_name):
+    """Returns full path to given app."""
     return join(get_apps_dir(), app_name)
 
 
 def make_run_script(run_config, run_script):
+    """Creates run scrtipt from ide launch script."""
     idea_script = get_launch_script(run_config.path_to_app)
 
     with open(idea_script, 'r') as src, open(run_script, 'w') as dst:
@@ -80,12 +89,14 @@ def make_run_script(run_config, run_script):
 
             dst.write(line)
 
-    st = stat(run_script)
-    chmod(run_script, st.st_mode | 0o0111)
+    stats = stat(run_script)
+    chmod(run_script, stats.st_mode | 0o0111)
 
 
 @dataclass()
 class ProductInfo:
+    """Ide product info"""
+    # pylint: disable=too-many-instance-attributes
     name: str
     version: str
     build_number: str
@@ -104,63 +115,68 @@ PRODUCT_INFO = 'product-info.json'
 
 @dataclass()
 class Version:
+    """Three-parts version representation."""
     year: int
     quart: int
     last: int
 
 
 def parse_version(version):
+    """Parses version string to Version class."""
     parsed = version.split(".")
-    v = Version(int(parsed[0]), int(parsed[1]), int(parsed[2] if len(parsed) > 2 else -1))
-
-    return v
+    return Version(int(parsed[0]), int(parsed[1]), int(parsed[2] if len(parsed) > 2 else -1))
 
 
 def get_data_dir_from_script(run_script):
-    with open(run_script, 'r') as f:
-        for line in f:
+    """Returns idea data dir from run script."""
+    with open(run_script, 'r') as file:
+        for line in file:
             pos = line.find(IDEA_PATH_SELECTOR)
 
             if pos != -1:
-                parts = line.split("=")
+                parts = line.split('=')
 
                 if len(parts) < 2:
-                    raise Exception(f"Unable to parse {IDEA_PATH_SELECTOR} line.")
+                    raise Exception(f'Unable to parse {IDEA_PATH_SELECTOR} line.')
 
-                return parts[1].split(" ")[0]
+                return parts[1].split(' ')[0]
 
-    raise Exception("Unable to find data directory in the launch script.")
+    raise Exception('Unable to find data directory in the launch script.')
 
 
 def get_product_info(app_path):
+    """Parses product info file to ProductInfo class."""
     prod_info_path = join(app_path, PRODUCT_INFO)
-    with open(prod_info_path, "r") as f:
-        data = json.load(f)
+    with open(prod_info_path, "r") as file:
+        data = json.load(file)
 
-        pi = ProductInfo(data['name'], data['version'], data['buildNumber'],
-                         data['productCode'], '', data['svgIconPath'],
-                         data['launch'][0]['os'],
-                         data['launch'][0]['launcherPath'],
-                         data['launch'][0]['javaExecutablePath'],
-                         data['launch'][0]['vmOptionsFilePath'],
-                         data['launch'][0]['startupWmClass'])
+        product_info = ProductInfo(data['name'], data['version'], data['buildNumber'],
+                                   data['productCode'], '', data['svgIconPath'],
+                                   data['launch'][0]['os'],
+                                   data['launch'][0]['launcherPath'],
+                                   data['launch'][0]['javaExecutablePath'],
+                                   data['launch'][0]['vmOptionsFilePath'],
+                                   data['launch'][0]['startupWmClass'])
 
-        v = parse_version(pi.version)
+        version = parse_version(product_info.version)
 
-        if v.year >= 2020 and v.quart >= 2:
-            pi.data_dir = data['dataDirectoryName']
+        if version.year >= 2020 and version.quart >= 2:
+            product_info.data_dir = data['dataDirectoryName']
         else:
-            pi.data_dir = get_data_dir_from_script(join(app_path, pi.launcher_path))
+            product_info.data_dir = get_data_dir_from_script(
+                join(app_path, product_info.launcher_path))
 
-        return pi
+        return product_info
 
 
 def get_launch_script(app_path):
+    """Returns full path to launch script by ide path."""
     prod_info = get_product_info(app_path)
     return join(app_path, prod_info.launcher_path)
 
 
 def get_bin_dir(app_path):
+    """Get full path to ide bin dir."""
     run_script = get_launch_script(app_path)
     return dirname(run_script)
 
@@ -170,23 +186,25 @@ VER_2020_CONFIG_PREFIX = expanduser('~/.config/JetBrains')
 
 
 def get_config_dir(app_path):
-    pi = get_product_info(app_path)
-    v = parse_version(pi.version)
+    """Returns ide config directory."""
+    product_info = get_product_info(app_path)
+    version = parse_version(product_info.version)
 
-    if v.year >= 2020:
-        return join(VER_2020_CONFIG_PREFIX, pi.data_dir)
+    if version.year >= 2020:
+        return join(VER_2020_CONFIG_PREFIX, product_info.data_dir)
 
-    return join(join(CONFIG_PREFIX, '.' + pi.data_dir), 'config')
+    return join(join(CONFIG_PREFIX, '.' + product_info.data_dir), 'config')
 
 
 PLUGIN_2020_PREFIX = expanduser('~/.local/share/JetBrains')
 
 
 def get_plugin_dir(app_path):
-    pi = get_product_info(app_path)
-    v = parse_version(pi.version)
+    """Returns full path to application plugin directory."""
+    product_info = get_product_info(app_path)
+    version = parse_version(product_info.version)
 
-    if v.year >= 2020:
-        return join(PLUGIN_2020_PREFIX, pi.data_dir)
+    if version.year >= 2020:
+        return join(PLUGIN_2020_PREFIX, product_info.data_dir)
 
     return join(get_config_dir(app_path), "plugins")
