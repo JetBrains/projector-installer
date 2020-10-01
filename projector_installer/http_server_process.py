47b756eb197f819bc86822060fbb9f6a141396b7
Ilya Krukov <ikrukov@horis.ru>
2020-10-01 17:17:34 +0300

    Remove host arg from redirect url

--- projector_installer/http_server_process.py
+++ projector_installer/http_server_process.py
@@ -53,10 +53,10 @@ class NoLogServer(SimpleHTTPRequestHandler):
 
         if len(cls.token) > 0:  # secure connection
             return f'https://{cls.address}:{cls.port}/index.html?' \
-                   f'host={cls.address}&port={cls.projector_port}'
+                   f'port={cls.projector_port}'
 
         return f'http://{cls.address}:{cls.port}/index.html?' \
-               f'host={cls.address}&port={cls.projector_port}'
+               f'port={cls.projector_port}'
 
     def is_empty_path(self) -> bool:
         """Checks if current path is empty."""
