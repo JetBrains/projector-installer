78eb449031a52c089314761b39f2145c220b80da
Ilya Krukov <ikrukov@horis.ru>
2020-12-24 15:20:04 +0300

    Add IDEKind to InstallableApp

--- projector_installer/installable_app.py
+++ projector_installer/installable_app.py
@@ -4,18 +4,35 @@
 
 """InstallableApp class and related stuff"""
 import json
-from typing import List, Tuple
+from typing import List, Tuple, Any
+from enum import Enum, auto
+
+
+class IDEKind(Enum):
+    """Known IDE kinds"""
+    Unknown = auto()
+    Idea_Community = auto()
+    Idea_Ultimate = auto()
+    PyCharm_Community = auto()
+    PyCharm_Professional = auto()
+    CLion = auto()
+    GoLand = auto()
+    DataGrip = auto()
+    PhpStorm = auto()
+    WebStorm = auto()
+    RubyMine = auto()
 
 
 class InstallableApp:
     """Installable application entry."""
 
-    def __init__(self, name: str, url: str) -> None:
+    def __init__(self, name: str, url: str, kind: IDEKind) -> None:
         self.name: str = name
         self.url: str = url
+        self.kind = kind
 
-    def __key__(self) -> Tuple[str, str]:
-        return self.name, self.url
+    def __key__(self) -> Tuple[str, str, IDEKind]:
+        return self.name, self.url, self.kind
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, InstallableApp):
@@ -27,9 +44,20 @@ class InstallableApp:
         return hash(self.__key__())
 
 
+def _parse_entry(entry: Any) -> InstallableApp:
+    """Get InstallableApp from JSON array entry"""
+
+    try:
+        kind = IDEKind[entry['kind']]
+    except KeyError:
+        kind = IDEKind.Unknown
+
+    return InstallableApp(entry['name'], entry['url'], kind)
+
+
 def load_installable_apps(file_name: str) -> List[InstallableApp]:
     """Loads installable app list from json file."""
     with open(file_name, 'r') as file:
         data = json.load(file)
 
-    return [InstallableApp(entry['name'], entry['url']) for entry in data]
+    return [_parse_entry(entry) for entry in data]
