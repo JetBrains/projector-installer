99f99c95e256c07b9fb7e4d8bb7cb8a592fb21b2
Georgii <georgii.kashin@jetbrains.com>
2021-07-27 16:44:17 +0300

    Refactored test's code to satisfy pylint's requirements

--- test/dialogs_test.py
+++ test/dialogs_test.py
@@ -12,7 +12,7 @@ from projector_installer.dialogs import get_user_input, is_boolean_input, ask, \
 class DialogsTests(TestCase):
     """Test dialogs.py module"""
 
-    def test_get_user_input(self): # type: ignore
+    def test_get_user_input(self):  # type: ignore
         """The get_user_input method must return the user's input"""
         some_input = 'some_input'
         original_input = mock.builtins.input
@@ -20,14 +20,14 @@ class DialogsTests(TestCase):
         self.assertEqual(get_user_input('prompt', 'default'), some_input)
         mock.builtins.input = original_input
 
-    def test_get_user_input_default(self): # type: ignore
+    def test_get_user_input_default(self):  # type: ignore
         """The get_user_input method must return default if the input is empty"""
         original_input = mock.builtins.input
         mock.builtins.input = lambda _: ''
         self.assertEqual(get_user_input('prompt', 'default'), 'default')
         mock.builtins.input = original_input
 
-    def test_is_boolean_input_true(self): # type: ignore
+    def test_is_boolean_input_true(self):  # type: ignore
         """
         The is_boolean_input method must return true
         if the user's input is in ['Y', 'y', 'N', 'n']
@@ -37,7 +37,7 @@ class DialogsTests(TestCase):
         self.assertTrue(is_boolean_input('N'))
         self.assertTrue(is_boolean_input('n'))
 
-    def test_is_boolean_input_false(self): # type: ignore
+    def test_is_boolean_input_false(self):  # type: ignore
         """
         The is_boolean_input method must return false
         if the user's input is not in ['Y', 'y', 'N', 'n']
@@ -46,28 +46,28 @@ class DialogsTests(TestCase):
         self.assertFalse(is_boolean_input('true'))
         self.assertFalse(is_boolean_input(''))
 
-    def test_ask_yes(self): # type: ignore
+    def test_ask_yes(self):  # type: ignore
         """The ask method must return true if the user's input is 'y'"""
         original_input = mock.builtins.input
         mock.builtins.input = lambda _: 'y'
         self.assertTrue(ask('prompt', True))
         mock.builtins.input = original_input
 
-    def test_ask_no(self): # type: ignore
+    def test_ask_no(self):  # type: ignore
         """The ask method must return false if the user's input is 'n'"""
         original_input = mock.builtins.input
         mock.builtins.input = lambda _: 'n'
         self.assertFalse(ask('prompt', False))
         mock.builtins.input = original_input
 
-    def test_ask_empty(self): # type: ignore
+    def test_ask_empty(self):  # type: ignore
         """The ask method must return true if the user's input is empty"""
         original_input = mock.builtins.input
         mock.builtins.input = lambda _: ''
         self.assertTrue(ask('prompt', True))
         mock.builtins.input = original_input
 
-    def test_prompt_with_default(self): # type: ignore
+    def test_prompt_with_default(self):  # type: ignore
         """The prompt_with_default method must return the user's input"""
         some_input = 'non empty input'
         original_input = mock.builtins.input
@@ -75,7 +75,7 @@ class DialogsTests(TestCase):
         self.assertEqual(prompt_with_default(prompt='prompt', default='default'), some_input)
         mock.builtins.input = original_input
 
-    def test_prompt_with_default_empty_input(self): # type: ignore
+    def test_prompt_with_default_empty_input(self):  # type: ignore
         """The prompt_with_default method must return 'default' if the user's input is empty"""
         empty_input = ''
         original_input = mock.builtins.input
