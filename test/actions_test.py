3c46f7cef37ebaad0647e0cb8cc50be9abeb529f
Georgii <georgii.kashin@jetbrains.com>
2021-07-28 12:06:13 +0300

    Removed location dependent tests. Refactored some tests

--- test/actions_test.py
+++ test/actions_test.py
@@ -3,13 +3,16 @@ from unittest import TestCase
 import pytest
 
 from projector_installer.actions import is_compatible_java
+from projector_installer.apps import UnknownIDEException
 
 
 class ActionsTest(TestCase):
     """Test actions.py module"""
 
-    def test_is_compatible_java_false(self):
-        """The is_compatible_java method must raise an exception
-        if the input app path is incorrect"""
-        with pytest.raises(Exception):
+    def test_is_compatible_java_raises_exception(self):
+        """
+        The is_compatible_java method must raise an exception
+        if the input app path is incorrect
+        """
+        with pytest.raises(UnknownIDEException):
             is_compatible_java("incorrect_app_path")
