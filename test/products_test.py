d0d9449ef365d749c273a61548df7035f8ffdfd0
Georgii <georgii.kashin@jetbrains.com>
2021-08-27 15:42:23 +0300

    Added unit tests

---
+++ test/products_test.py
@@ -0,0 +1,42 @@
+"""Test products.py module"""
+from unittest import TestCase
+import os
+
+from test.resources.installable_apps import INSTALLABLE_APPS
+from projector_installer.products import load_installable_apps_from_file, get_all_product_codes, \
+    filter_app_by_name_pattern
+from projector_installer.products import Product, IDEKind
+
+class ProductsTest(TestCase):
+    """Test products.py module"""
+
+    def test_load_installable_apps_from_file(self) -> None:
+        """The load_installable_apps_from_file method must load apps from json file"""
+        compatible_ide_json_path = os.path.abspath("test/resources/compatible_ide.json")
+        self.assertEqual(load_installable_apps_from_file(compatible_ide_json_path),
+                         INSTALLABLE_APPS)
+
+    def test_get_all_product_codes(self) -> None:
+        """The get_all_product_codes method must return product codes"""
+        all_product_codes = 'code=IIC&code=IIU&code=PCC&code=PCP&code=CL&code=GO&code=DG&code=PS&' \
+                            'code=WS&code=RM&code=RD&code=PD&code=MPS'
+        self.assertEqual(get_all_product_codes(), all_product_codes)
+
+    def test_filter_app_by_name_pattern_exact_match(self) -> None:
+        """The filter_app_by_name_pattern method must filter apps by given name"""
+        matched_product = [Product('IntelliJ IDEA Community 2020.1.1',
+                                   'https://download.jetbrains.com/idea/ideaIC-2020.1.1.tar.gz',
+                                   IDEKind.Idea_Community)]
+        self.assertEqual(filter_app_by_name_pattern(INSTALLABLE_APPS,
+                                                    'IntelliJ IDEA Community 2020.1.1'
+                                                    ), matched_product)
+
+    def test_filter_app_by_name_pattern_no_pattern(self) -> None:
+        """The filter_app_by_name_pattern method must filter nothing when no pattern provided"""
+        self.assertEqual(filter_app_by_name_pattern(INSTALLABLE_APPS), INSTALLABLE_APPS)
+
+    def test_filter_app_by_name_pattern_not_matched_pattern(self) -> None:
+        """
+        The filter_app_by_name_pattern method must return empty array when pattern matched nothing
+        """
+        self.assertEqual(filter_app_by_name_pattern(INSTALLABLE_APPS, 'not_matched_pattern'), [])
---
+++ test/secure_config_test.py
@@ -0,0 +1,135 @@
+"""Test secure_config.py module"""
+import os
+from os.path import expanduser
+
+from unittest import TestCase
+
+from projector_installer.secure_config import get_ca_ini_file, create_ca_ini, \
+    get_ca_jks_backup_file, make_ca_backup, restore_ca_backup, remove_ca_backup, get_ca_password
+from projector_installer.version import __version__
+
+class SecureConfigTest(TestCase):
+    """Test secure_config.py module"""
+
+    user = expanduser('~')
+
+    def test_get_ca_ini_file(self) -> None:
+        """The get_ca_ini_file method must return a path to ca.ini file"""
+        ca_ini_file = f'{self.user}/.projector/ssl/ca.ini'
+        self.assertEqual(get_ca_ini_file(), ca_ini_file)
+
+    def test_create_ca_ini(self) -> None:
+        """The create_ca_ini method must create ca.ini file with provided token"""
+        ssl_dir_path = f'{self.user}/.projector/ssl'
+        os.mkdir(ssl_dir_path)
+
+        ca_ini_file = f'{self.user}/.projector/ssl/ca.ini'
+        open(ca_ini_file, 'a').close()
+
+        create_ca_ini(token='token')
+
+        with open(ca_ini_file, 'r') as configfile:
+            self.assertEqual(configfile.read(), '[CA]\nsaved = token\n\n')
+
+        os.remove(ca_ini_file)
+        os.rmdir(ssl_dir_path)
+
+    def test_get_ca_jks_backup_file(self) -> None:
+        """The get_ca_jks_backup_file method must return a path to ca.jks.backup file"""
+        ca_jks_backup_file = f'{self.user}/.projector/ssl/ca.jks.backup'
+        self.assertEqual(get_ca_jks_backup_file(), ca_jks_backup_file)
+
+    def test_make_ca_backup(self) -> None:
+        """The make_ca_backup method must create a backup of ca.jks file"""
+        ssl_dir_path = f'{self.user}/.projector/ssl'
+        os.mkdir(ssl_dir_path)
+
+        ca_jks_file = f'{self.user}/.projector/ssl/ca.jks'
+        open(ca_jks_file, 'w').close()
+
+        self.assertTrue(make_ca_backup())
+
+        ca_jks_backup_file = f'{self.user}/.projector/ssl/ca.jks.backup'
+        self.assertTrue(os.path.exists(ca_jks_backup_file))
+
+        os.remove(ca_jks_file)
+        os.remove(ca_jks_backup_file)
+        os.rmdir(ssl_dir_path)
+
+    def test_make_ca_backup_no_file_exists(self) -> None:
+        """The make_ca_backup method must return false when source file doesn't exist"""
+        ssl_dir_path = f'{self.user}/.projector/ssl'
+        os.mkdir(ssl_dir_path)
+
+        self.assertFalse(make_ca_backup())
+
+        os.rmdir(ssl_dir_path)
+
+    def test_restore_ca_backup(self) -> None:
+        """The restore_ca_backup method must restore ca.jks file from backup"""
+        ssl_dir_path = f'{self.user}/.projector/ssl'
+        os.mkdir(ssl_dir_path)
+
+        ca_jks_backup_file = f'{self.user}/.projector/ssl/ca.jks.backup'
+        open(ca_jks_backup_file, 'w').close()
+
+        self.assertTrue(restore_ca_backup())
+
+        ca_jks_file = f'{self.user}/.projector/ssl/ca.jks'
+        self.assertTrue(os.path.exists(ca_jks_file))
+
+        os.remove(ca_jks_file)
+        os.remove(ca_jks_backup_file)
+        os.rmdir(ssl_dir_path)
+
+    def test_restore_ca_backup_no_backup_file_exists(self) -> None:
+        """The restore_ca_backup method must return false when backup file doesn't exist"""
+        ssl_dir_path = f'{self.user}/.projector/ssl'
+        os.mkdir(ssl_dir_path)
+
+        self.assertFalse(restore_ca_backup())
+
+        os.rmdir(ssl_dir_path)
+
+    def test_remove_ca_backup(self) -> None:
+        """
+        The remove_ca_backup method must return true when deletion of backup file was successful
+        """
+        ssl_dir_path = f'{self.user}/.projector/ssl'
+        os.mkdir(ssl_dir_path)
+
+        ca_jks_backup_file = f'{self.user}/.projector/ssl/ca.jks.backup'
+        open(ca_jks_backup_file, 'w').close()
+
+        self.assertTrue(remove_ca_backup())
+        self.assertFalse(os.path.exists(ca_jks_backup_file))
+
+        os.rmdir(ssl_dir_path)
+
+    def test_remove_ca_backup_no_backup_file_exists(self) -> None:
+        """The remove_ca_backup method must return false when backup file doesn't exist"""
+        ssl_dir_path = f'{self.user}/.projector/ssl'
+        os.mkdir(ssl_dir_path)
+
+        self.assertFalse(remove_ca_backup())
+
+        os.rmdir(ssl_dir_path)
+
+    def test_get_ca_password(self) -> None:
+        """The get_ca_password method must return ca password"""
+        self.assertEqual(get_ca_password(), '85TibAyPS3NZX3')
+
+    def test_get_ca_password_no_ca_ini_file_exists(self) -> None:
+        """The get_ca_password method must return token located in ca.ini file"""
+        ssl_dir_path = f'{self.user}/.projector/ssl'
+        os.mkdir(ssl_dir_path)
+
+        ca_ini_file = f'{self.user}/.projector/ssl/ca.ini'
+        open(ca_ini_file, 'a').close()
+
+        create_ca_ini(token='token')
+
+        self.assertEqual(get_ca_password(), 'token')
+
+        os.remove(ca_ini_file)
+        os.rmdir(ssl_dir_path)
