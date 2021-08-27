8d97a8c43e8409665cfebf6fcb6cc5959d902d90
Georgii <georgii.kashin@jetbrains.com>
2021-08-27 15:42:23 +0300

    Added unit tests

---
+++ test/projector_updates_test.py
@@ -0,0 +1,28 @@
+"""Test projector_updates.py module"""
+from unittest import TestCase
+
+from projector_installer.projector_updates import is_newer_than_current
+from projector_installer.version import __version__
+
+class ProjectorUpdatesTest(TestCase):
+    """Test projector_updates.py module"""
+
+    def test_is_newer_than_current_true(self) -> None:
+        """The is_newer_than_current method must return true if a newer version is provided"""
+        first_digit = __version__[0:__version__.find('.')]
+        new_first_digit = int(first_digit) + 1
+        newer_version = __version__.replace(first_digit, str(new_first_digit))
+
+        self.assertTrue(is_newer_than_current(newer_version))
+
+    def test_is_newer_than_current_false(self) -> None:
+        """The is_newer_than_current method must return false if an older version is provided"""
+        first_digit = __version__[0:__version__.find('.')]
+        new_first_digit = int(first_digit) - 1
+        newer_version = __version__.replace(first_digit, str(new_first_digit))
+
+        self.assertFalse(is_newer_than_current(newer_version))
+
+    def test_is_newer_than_current_same_version(self) -> None:
+        """The is_newer_than_current method must return false if the same version is provided"""
+        self.assertFalse(is_newer_than_current(__version__))
---
+++ test/run_config_test.py
@@ -0,0 +1,92 @@
+"""Test run_config.py module"""
+from os.path import expanduser
+import os
+
+from unittest import TestCase
+import pytest
+
+from projector_installer.run_config import get_path_to_config, get_path_to_certificate_dir, \
+    get_run_script_path, make_config_name, RunConfig, validate_run_config, \
+    get_lock_file_name, lock_config
+from projector_installer.version import __version__
+
+
+class RunConfigTest(TestCase):
+    """Test run_config.py module"""
+
+    user = expanduser('~')
+
+    def test_get_path_to_config(self) -> None:
+        """The get_path_to_config method must return path to config with a given name"""
+        config_name = 'config_name'
+        path_to_config = f'{self.user}/.projector/configs/{config_name}'
+        self.assertEqual(get_path_to_config(config_name), path_to_config)
+
+    def test_get_path_to_certificate_dir(self) -> None:
+        """
+        The get_path_to_certificate_dir method must return path to certificate dir of given config
+        """
+        config_name = 'config_name'
+        path_to_certificate_dir = f'{self.user}/.projector/configs/{config_name}/cert'
+        self.assertEqual(get_path_to_certificate_dir(config_name), path_to_certificate_dir)
+
+    def test_get_run_script_path(self) -> None:
+        """The get_run_script_path method must return path to run script of given config name"""
+        config_name = 'config_name'
+        run_script_path = f'{self.user}/.projector/configs/{config_name}/run.sh'
+        self.assertEqual(get_run_script_path(config_name), run_script_path)
+
+    def test_make_config_name(self) -> None:
+        """
+        The make_config_name method must return the same app name as provided if there is no space
+        """
+        app_name = 'app_name'
+        self.assertEqual(make_config_name(app_name), 'app_name')
+
+    def test_make_config_name_with_space(self) -> None:
+        """The make_config_name method must return the first part of app name when name has space"""
+        app_name = 'app name'
+        self.assertEqual(make_config_name(app_name), 'app')
+
+    def test_validate_run_config_no_errors(self) -> None:
+        """The validate_run_config method must not raise error when run config is valid"""
+        app_dir_path = f'{self.user}/.projector/apps/app_dir'
+        os.mkdir(app_dir_path)
+        run_config = RunConfig(name='name', path_to_app=app_dir_path, projector_port=8887,
+                               token='token', password='password', ro_password='ro_password',
+                               toolbox=True, custom_names='custom_names')
+
+        validate_run_config(run_config)
+        os.rmdir(app_dir_path)
+
+    def test_validate_run_config_raises_error(self) -> None:
+        """The validate_run_config method must raise error when path to app is invalid"""
+        run_config = RunConfig(name='name', path_to_app='path_to_app', projector_port=8887,
+                               token='token', password='password', ro_password='ro_password',
+                               toolbox=True, custom_names='custom_names')
+
+        with pytest.raises(ValueError):
+            validate_run_config(run_config)
+
+    def test_get_lock_file_name(self) -> None:
+        """The get_lock_file_name method must return a path to lock file for given config name"""
+        config_name = 'config_name'
+        lock_file_name = f'{self.user}/.projector/configs/{config_name}/run.lock'
+        self.assertEqual(get_lock_file_name(config_name), lock_file_name)
+
+    def test_lock_config(self) -> None:
+        """The lock_config method must return the TextIO of file if lock was successful"""
+        config_name = 'config_name'
+        config_dir_path = f'{self.user}/.projector/configs/{config_name}'
+        os.mkdir(config_dir_path)
+
+        lock_file_path = f'{config_dir_path}/run.lock'
+        open(lock_file_path, 'w').close()
+        output = lock_config(config_name)
+
+        expected_output = f"<_io.TextIOWrapper name='{self.user}/.projector/configs/" \
+                          f"config_name/run.lock' mode='w' encoding='UTF-8'>"
+        self.assertEqual(str(output), expected_output)
+
+        os.remove(lock_file_path)
+        os.rmdir(config_dir_path)
